c6c852dd6d3fe8280043cc8a5f4fe9842ad1cbaa
ApCsUses <s-daik@bsd405.org>
2021-11-02 14:32:17 -0700

    add fab, popup layout, save button, and associate fab with creating the popup

--- app/src/main/java/com/kdbl/mywatchlist/MainActivity.java
+++ app/src/main/java/com/kdbl/mywatchlist/MainActivity.java
@@ -7,6 +7,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -22,7 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
         mDbOpenHelper = new WatchListOpenHelper(this);
 
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "clicked fab", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                onButtonShowPopupClick(v);
+            }
+        });
+
         initializeDisplayContent();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mDbOpenHelper.close();
+        super.onDestroy();
     }
 
     private void initializeDisplayContent() {
@@ -37,9 +62,35 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(animeRecyclerAdapter);
     }
 
-    @Override
-    protected void onDestroy() {
-        mDbOpenHelper.close();
-        super.onDestroy();
+    public void onButtonShowPopupClick(View view) {
+//        inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View newAnimePopup = inflater.inflate(R.layout.new_anime_popup, null);
+
+//        create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        final PopupWindow animePopup = new PopupWindow(newAnimePopup, width, height, focusable);
+
+//        show the popup window
+        animePopup.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        Button saveButton = newAnimePopup.findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "clicked save", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        animePopup.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                animePopup.dismiss();
+            }
+        });
     }
 }
