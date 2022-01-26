e7e35e163556f6a44e2db97a454c833cb184523b
ApCsUses <s-daik@bsd405.org>
2022-01-25 16:40:14 -0800

    fix data scraping for myanimelist.net

--- app/src/main/java/com/kdbl/mywatchlist/DataScraper.java
+++ app/src/main/java/com/kdbl/mywatchlist/DataScraper.java
@@ -2,8 +2,6 @@ package com.kdbl.mywatchlist;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,53 +14,66 @@ import org.jsoup.select.Elements;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class DataScraper implements Executor {
 
-    public static final int NUM_DATA = 8;
+    public static final int MAX_ITERATION_TO_CONSIDER = 25;
     private final String mURL;
+    private Set<String> mInfoTags;
 
     public DataScraper(String URL) {
         mURL = URL;
+        mInfoTags = new HashSet<>();
+        mInfoTags.add("English");
+        mInfoTags.add("Episodes");
+        mInfoTags.add("Status");
+        mInfoTags.add("Aired");
     }
 
     public void populateAnimeInfo(View urlDisplayView, boolean isNew) {
         this.execute(new Runnable() {
             @Override
             public void run() {
-                String[] data = new String[NUM_DATA];
                 try {
+//                    Access airing info
+                    Map<String, String> displayInfo = new HashMap<>();
                     Document document = Jsoup.connect(mURL).get();
                     Elements animeInfo = document.select("div.spaceit_pad");
                     Iterator<Element> infoIterator = animeInfo.iterator();
                     int i = 0;
-                    while(infoIterator.hasNext() && i < NUM_DATA) {
-                        String temp = infoIterator.next().text();
-                        data[i] = temp.split(":")[1].trim();
+                    while(infoIterator.hasNext() && i < MAX_ITERATION_TO_CONSIDER) {
+                        String[] temp = infoIterator.next().text().split(":");
+                        if(mInfoTags.contains(temp[0])) {
+                            displayInfo.put(temp[0], temp[1]);
+                        }
                         i++;
                     }
+//                    Access anime name
+                    Element animeTitle = document.selectFirst("h1.title-name.h1_bold_none");
 //                    Populate view
                     if(isNew) {
                         urlDisplayView.findViewById(R.id.editTextTitle).post(() ->
                                 ((TextView) urlDisplayView.findViewById(R.id.editTextTitle))
-                                        .setText(data[0]));
+                                        .setText(animeTitle.text()));
                     }
                     urlDisplayView.findViewById(R.id.englishTextView).post(() ->
                             ((TextView) urlDisplayView.findViewById(R.id.englishTextView))
-                                    .setText(data[2]));
+                                    .setText("English: " + displayInfo.get("English")));
                     urlDisplayView.findViewById(R.id.episodeTextView).post(() ->
                             ((TextView) urlDisplayView.findViewById(R.id.episodeTextView))
-                                    .setText(data[4]));
+                                    .setText("Episodes: " + displayInfo.get("Episodes")));
                     urlDisplayView.findViewById(R.id.statusTextView).post(() ->
                             ((TextView) urlDisplayView.findViewById(R.id.statusTextView))
-                                    .setText(data[5]));
+                                    .setText("Status: " + displayInfo.get("Status")));
                     urlDisplayView.findViewById(R.id.airingTextView).post(() ->
                             ((TextView) urlDisplayView.findViewById(R.id.airingTextView))
-                                    .setText(data[6]));
+                                    .setText("Aired: " + displayInfo.get("Aired")));
                 } catch (Exception exception) {
                     exception.printStackTrace();
                     Log.w("populateAnimeInfo", exception.toString());
